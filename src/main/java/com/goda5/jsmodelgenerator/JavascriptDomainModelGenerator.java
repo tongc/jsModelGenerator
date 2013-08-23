4d8c073dfcbb7f364ea896109831c12098fc9753
chentong <chentongtc@gmail.com>
2013-08-24 00:03:47 +0100

    updated model generator to have comments for array type

--- src/main/java/com/goda5/jsmodelgenerator/JavascriptDomainModelGenerator.java
+++ src/main/java/com/goda5/jsmodelgenerator/JavascriptDomainModelGenerator.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.Collection;
 
 public class JavascriptDomainModelGenerator {
 	public void generate(Class<?> clazz, File targetJsFolder) throws Exception {
@@ -15,13 +16,16 @@ public class JavascriptDomainModelGenerator {
 			for(Field field:clazz.getDeclaredFields()) {
 				if(!Modifier.isStatic(field.getModifiers()) && !Modifier.isFinal(field.getModifiers())) {
 					b.append(prefix);
-					prefix = ", ";
+					prefix = "";
 					b.append(field.getName());
 				}
 			}
 			b.append(") {" + System.getProperty("line.separator"));
 			for(Field field:clazz.getDeclaredFields()) {
 				if(!Modifier.isStatic(field.getModifiers()) && !Modifier.isFinal(field.getModifiers())) {
+					if(field.getType().isAssignableFrom(Collection.class)) {
+						b.append("//Array" + System.getProperty("line.separator"));
+					}
 					b.append("    this." + field.getName() + " = " + field.getName() + suffix + System.getProperty("line.separator"));
 				}
 			}
